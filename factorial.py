0fbc639d4965523be095f30af9e16854da00be6d
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2012-09-12 15:56:43 +0400

    Add docstrings

--- factorial.py
+++ factorial.py
@@ -4,21 +4,22 @@
 ## Internal functions
 ############################################################
 
-## Print script usage
 def usage():
+    "Print script usage."
     print """
 Usage: %s <integer>
     Print the factorial of the <integer>.
 """ % argv[0],
 
-
-## Return the factorial of the integer
 def factorial(integer):
+    "Return the factorial of the integer."
     if 0 > integer:
         exit('Error: value must be greater or equal to 0.')
     return(factorial_impl(integer, 1))
 
 def factorial_impl(i, acc):
+    """Return the factorial of the integer.
+    Recursive implementation, use factorial/1."""
     if 0 == i:
         return acc
     return factorial_impl(i - 1, i * acc)
@@ -31,6 +32,8 @@ def factorial_impl(i, acc):
 all_tests_pass = True
 
 def assert_equal(expected, form):
+    """Assert equality of the form evaluation and expected value.
+    In case of failure trigger all_tests_pass global variable to False."""
     value = eval(form)
     if expected != value:
         global all_tests_pass
@@ -43,21 +46,26 @@ value: %r
 
 
 def tests():
+    "Run all internal tests."
     assert_equal(479001600, "factorial(12)")
 
+
 def check():
+    """Check module by running all internal tests.
+    Exit script if something fail."""
     global all_tests_pass
     all_tests_pass = True
     tests()
     if False == all_tests_pass:
         exit("Self check failed, can't continue!")
 
-# Running tests
-check()
 
 ############################################################
 ## Script 
 ############################################################
+
+# Running tests
+check()
 
 from sys import argv
 
